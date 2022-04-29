a4dd148831b7a2ea282693e3eadeaee148d3b284
GatsbyJr <gocha.berulava@gmail.com>
2022-04-29 06:53:27 +0400

    Update Data.js

--- src/data/Data.js
+++ src/data/Data.js
@@ -41,82 +41,85 @@ const Data = ()=>{
                 setPlData(response.response)})
             .catch(err => console.error(err));
     },[])
-        let firstR = []
-        let secondR = []
-        let smth = []
-        useEffect(()=>{
-            const dataBase = {   
-            }
-            let roundName
-            if(roundsData){
-                roundsData.forEach(round=>{
-                    roundName=round
-                    smth=[]
-                    if(plData){
-                        plData.forEach(fixture=>{
-                            if(roundName == fixture.league.round){
-                                smth.push(fixture)
-                            }
+
+
+    let firstR = []
+    let secondR = []
+    let smth = []
+
+    const dataBase = {   
+    }
+    const getData = ()=>{
+        let roundName;
+        if(roundsData){
+            roundsData.forEach(round=>{
+                roundName=round
+                smth=[]
+                if(plData){
+                    plData.forEach(fixture=>{
+                        if(roundName == fixture.league.round){
+                            smth.push(fixture)
+                        }else return null
                     })
-                dataBase[roundName]=[smth]
-                }
-            })           
-             
+                dataBase.name = [roundName]
+                dataBase.fixtures= [smth]
+                }else return null 
+            })
+                         
             setDataWithRounds(dataBase)
-            }
-        },[])
-
-
-        const firstRound = ()=> {
-            if(plData){
-                plData.forEach(element => {               
-                    if(element.league.round=="Regular Season - 1"){
-                        firstR.push(element)
-                    }else if(element.league.round=="Regular Season - 2"){
-                        secondR.push(element)
-                    }
+            console.log(dataWithRounds)
+        }else return null     
+    }
+    useEffect(()=>{
+        getData()
+    },[roundsData])
+    const firstRound = ()=> {
+        if(plData){
+            plData.forEach(element => {               
+                if(element.league.round=="Regular Season - 1"){
+                    firstR.push(element)
+                }else if(element.league.round=="Regular Season - 2"){
+                    secondR.push(element)
                 }
-                )
             }
+            )
         }
+    }
 
 
-        firstRound()
 
-
-        const TableSchema = (props)=>{
-            return  <Table striped hover size="sm" variant="dark">
-                
-            <thead>
-                <tr className="match">
-                    <th> Date</th>
-                    <th></th>
-                    <th>{props.name}</th>
-                    <th></th>
-                    <th></th>
-                    <th>Venue</th>
-                </tr>
-            </thead>
-            <tbody>
-        {props.arr? props.arr.map((item,i)=>(              
-                <tr id={i} key={i} className="match">
-                    <td className="match_date">{item.fixture.date}</td>
-                    <td className="match_team team_home">  {item.teams.home.name} <img src={item.teams.home.logo}/> </td>
-                    <td className="match_score">{item.score.fulltime.home}</td>
-                    <td className="match_score">{item.score.fulltime.away}</td>
-                    <td className="match_team team_away"><img src={item.teams.away.logo}/> {item.teams.away.name} </td>
-                    <td className="match_date">{item.fixture.venue.name}</td>
-                </tr>
-            )):null}
-            </tbody>
-        </Table> 
-        }
+    const TableSchema = (props)=>{
+        return  <Table striped hover size="sm" variant="dark">
+            
+        <thead>
+            <tr className="match">
+                <th> Date</th>
+                <th></th>
+                <th>{props.name}</th>
+                <th></th>
+                <th></th>
+                <th>Venue</th>
+            </tr>
+        </thead>
+        <tbody>
+    {props.arr? props.arr.map((item,i)=>(              
+            <tr id={i} key={i} className="match">
+                <td className="match_date">{item.fixture.date}</td>
+                <td className="match_team team_home">  {item.teams.home.name} <img src={item.teams.home.logo}/> </td>
+                <td className="match_score">{item.score.fulltime.home}</td>
+                <td className="match_score">{item.score.fulltime.away}</td>
+                <td className="match_team team_away"><img src={item.teams.away.logo}/> {item.teams.away.name} </td>
+                <td className="match_date">{item.fixture.venue.name}</td>
+            </tr>
+        )):null}
+        </tbody>
+    </Table> 
+    }
 
 
 
     return(
         <div className="match_wrapper container-fluid">
-            {dataWithRounds? <div> asd asd</div>:null}
            
         </div>
     )
