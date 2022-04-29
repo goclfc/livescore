52c61efd52a0c98bd4137eb63cfbd5f05c15bb97
GatsbyJr <61660956+goclfc@users.noreply.github.com>
2022-04-29 16:30:32 +0400

    full season with rounds

--- src/data/Data.js
+++ src/data/Data.js
@@ -3,28 +3,6 @@ import './style.css'
 import { Table } from "react-bootstrap";
 const Data = ()=>{
     const [plData,setPlData] = useState(null)
-    const [roundsData,setRoundsData] = useState(null)
-    const [dataWithRounds,setDataWithRounds] = useState(null)
-
-
-    useEffect(()=>{
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com',
-                'X-RapidAPI-Key': 'a0bde3d87amsh3ceba186147e178p1b576djsn43ac8d69d928'
-            }
-        };
-        
-        fetch('https://api-football-v1.p.rapidapi.com/v3/fixtures/rounds?league=39&season=2020', options)
-            .then(response => response.json())
-            .then(response =>{
-                console.log(response)
-                setRoundsData(response.response)
-                })
-            .catch(err => console.error(err));
-
-    },[])
     
     useEffect(()=>{
         const options = {
@@ -42,60 +20,25 @@ const Data = ()=>{
             .catch(err => console.error(err));
     },[])
 
-
-    let firstR = []
-    let secondR = []
-    let smth = []
-
-    const dataBase = {   
-    }
-    const getData = ()=>{
-        let roundName;
-        if(roundsData){
-            roundsData.forEach(round=>{
-                roundName=round
-                smth=[]
-                if(plData){
-                    plData.forEach(fixture=>{
-                        if(roundName == fixture.league.round){
-                            smth.push(fixture)
-                        }else return null
-                    })
-                dataBase.name = [roundName]
-                dataBase.fixtures= [smth]
-                }else return null 
-            })
-                         
-            setDataWithRounds(dataBase)
-            console.log(dataWithRounds)
-        }else return null     
-    }
-    useEffect(()=>{
-        getData()
-    },[roundsData])
-    const firstRound = ()=> {
+    const getRound = (round)=>{
+        let searonRound = []
         if(plData){
-            plData.forEach(element => {               
-                if(element.league.round=="Regular Season - 1"){
-                    firstR.push(element)
-                }else if(element.league.round=="Regular Season - 2"){
-                    secondR.push(element)
+            plData.forEach(fixture=>{
+                if(fixture.league.round == round){
+                    searonRound.push(fixture)
                 }
-            }
-            )
+            })
+            return searonRound
         }
-    }
-
-
-
+    }   
     const TableSchema = (props)=>{
         return  <Table striped hover size="sm" variant="dark">
-            
+            <h5 style={{textAlign:"center"}}> {props.name}</h5>
         <thead>
             <tr className="match">
                 <th> Date</th>
                 <th></th>
-                <th>{props.name}</th>
+                <th></th>
                 <th></th>
                 <th></th>
                 <th>Venue</th>
@@ -103,8 +46,8 @@ const Data = ()=>{
         </thead>
         <tbody>
     {props.arr? props.arr.map((item,i)=>(              
-            <tr id={i} key={i} className="match">
-                <td className="match_date">{item.fixture.date}</td>
+        <tr id={i} key={i} className="match">
+            <td className="match_date">{item.fixture.date}</td>
                 <td className="match_team team_home">  {item.teams.home.name} <img src={item.teams.home.logo}/> </td>
                 <td className="match_score">{item.score.fulltime.home}</td>
                 <td className="match_score">{item.score.fulltime.away}</td>
@@ -115,12 +58,15 @@ const Data = ()=>{
         </tbody>
     </Table> 
     }
-
-
-
+    
+    
+    
     return(
         <div className="match_wrapper container-fluid">
-           
+            {plData? plData.map((fixture,i)=>(i<38?
+                <div><TableSchema name= {`Regular Season - Round ${i+1}`} arr={getRound(`Regular Season - ${i+1}`)}/></div>
+            :null)):null}
+
         </div>
     )
 }
